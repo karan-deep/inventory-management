4af6286a42d1c013e1afb643914de0c0f5b396ec
Karandeep Singh <skarandeep335@gmail.com>
2021-03-10 09:41:28 -0800

    Added function to createToken.

--- server/server.js
+++ server/server.js
@@ -10,3 +10,11 @@ const router = jsonServer.router("./server/database.json");
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(jsonServer.defaults());
+
+const key = "secret";
+
+const expiresIn = "2h";
+
+function createToken(payload) {
+  return jwt.sign(payload, key, { expiresIn });
+}
