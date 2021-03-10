b42dfb4b95ea05cf8b75a6015482a6f7b857a468
Karandeep Singh <skarandeep335@gmail.com>
2021-03-10 09:49:24 -0800

    Added login function in the auth-service.

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Login, Register, Token } from '../models/inventory';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  token: string;
+  isLoggedIn: boolean;
+  login(body: Login): Observable<Token> {
+    return this.http.post<Token>(environment.apiBasePath + 'auth/login', body);
+  }
+  constructor(private http: HttpClient, private router: Router) {
+  }
 }
