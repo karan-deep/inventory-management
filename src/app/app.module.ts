5ae5ef956fb553618a401ff716feee5134b3d6d7
Karandeep Singh <skarandeep335@gmail.com>
2021-03-10 09:55:45 -0800

    Declaring and importing shared module and other components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InventoriesComponent } from './components/inventories/inventories.component';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { InventoriesEditorComponent } from './components/inventories/inventories-editor/inventories-editor.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    RegisterComponent,
+    InventoriesComponent,
+    InventoriesEditorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
