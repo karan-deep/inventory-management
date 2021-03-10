b91e523540e11ef6e902db723efecff6a0cc182f
Karandeep Singh <skarandeep335@gmail.com>
2021-03-10 09:59:28 -0800

    Added error interceptor and logging out if the response status code is 401 or 403.

---
+++ src/app/shared/interceptors/error.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((err) => {
+        if ([401, 403].indexOf(err.status) !== -1) {
+          this.authService.logout();
+        }
+        return throwError(err.error);
+      })
+    );
+  }
+}
