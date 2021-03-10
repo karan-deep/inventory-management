fc0303b06c08244954e40aa355215425e86df038
Karandeep Singh <skarandeep335@gmail.com>
2021-03-10 09:50:53 -0800

    Calling login function and navigating to the inventory page on successful login.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Login } from 'src/app/shared/models/inventory';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  login: Login = {
+    email: '',
+    password: '',
+  };
+  error: string;
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
+
+  logIn() {
+    this.authService.login(this.login).subscribe(
+      (resultLogin) => {
+        this.authService.isLoggedIn = true;
+        this.authService.token = resultLogin.token;
+        this.router.navigate(['/inventory']);
+        localStorage.setItem('token', this.authService.token);
+      },
+      (error) => {
+        console.error(error);
+        this.error = error.message;
+      }
+    );
+  }
 }
