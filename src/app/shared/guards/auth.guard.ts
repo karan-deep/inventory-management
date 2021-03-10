f73b6f9c683690cb0b43c2c673ab59b076c4a20e
Karandeep Singh <skarandeep335@gmail.com>
2021-03-10 09:58:38 -0800

    Added authguard to protected the routes(private pages)

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  canActivate() {
+    if (!this.authService.isLoggedIn || !this.authService.token) {
+      this.authService.logout();
+      return false;
+    }
+    return true;
+  }
+}
