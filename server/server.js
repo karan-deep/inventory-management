9d502e84a6b1c994482f3027177e709d08b1f6bd
Karandeep Singh <skarandeep335@gmail.com>
2021-03-10 09:40:59 -0800

    Importing libraries and creating a server.

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const jsonServer = require("json-server");
+const jwt = require("jsonwebtoken");
+
+const server = jsonServer.create();
+const router = jsonServer.router("./server/database.json");
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(jsonServer.defaults());
